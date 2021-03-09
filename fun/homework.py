192e55eb12b4d8bcc67f32bbc9524ff5f768ff7c
vcu-venablean <venablean@vcu.edu>
2021-03-08 20:23:16 -0500

    homework file update

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    pass
+    return max(incoming_list)
 
 
 def find_least_number(incoming_list):
@@ -14,7 +14,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    pass
+    return min(incoming_list)
 
 
 def add_list_numbers(incoming_list):
@@ -23,7 +23,7 @@ def add_list_numbers(incoming_list):
     Add all the values together and return it.
     """
 
-    pass
+    return sum(incoming_list)
 
 
 def longest_value_key(incoming_dict):
@@ -31,4 +31,4 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    pass
+    return max(incoming_dict.keys(), key = len)
