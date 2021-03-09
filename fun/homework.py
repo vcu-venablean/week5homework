1373f9810281591ffe5ec4b144243d2957a2459c
vcu-venablean <venablean@vcu.edu>
2021-03-08 21:07:35 -0500

    hw update

--- fun/homework.py
+++ fun/homework.py
@@ -6,6 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+
     return max(incoming_list)
 
 
@@ -14,6 +15,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
+
     return min(incoming_list)
 
 
